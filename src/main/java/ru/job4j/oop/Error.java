d10c95c86cdbf7f3900d1533ea637406a5e21a18
Lera <valeriavovnenko0@gmail.com>
2022-05-06 21:32:18 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Error {
+
+        private boolean active;
+        private int status;
+        private String message;
+
+
+        public Error() {
+
+     }
+
+     public Error(boolean active, int status, String message) {
+
+            this.active = active;
+            this.status = status;
+            this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 1, "Alarm");
+        error.printInfo();
+        Error error1 = new Error(false, 0, "Hello World!");
+        error1.printInfo();
+        Error error2 = new Error(true, 5, "I like Java");
+        error2.printInfo();
+        Error error3 = new Error();
+        error3.printInfo();
+    }
+     public void printInfo() {
+         System.out.println("Active: " + active);
+         System.out.println("Status: " + status);
+         System.out.println("Message: " + message);
+     }
+}
