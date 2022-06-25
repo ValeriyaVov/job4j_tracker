d0a50ebff10271bc77eca26884cff41c76cc3c3c
Lera <valeriavovnenko0@gmail.com>
2022-06-25 14:34:13 +0300

    0. Что такое исключение. [#3821 #316062]

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class BackArray {
     public static void main(String[] args) {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
-        for (int index = 0; index < names.length; index++) {
+        int middle = names.length / 2;
+        for (int index = 0; index <= middle; index++) {
             String temp = names[index];
             names[index] = names[names.length - 1];
             names[names.length - 1] = temp;
